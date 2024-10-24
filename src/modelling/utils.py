ea502dcfe883f0f203e2e949186eecbb6c219083
fmansilla10 <144461931+fmansilla10@users.noreply.github.com>
2024-10-24 15:16:55 +0200

    from_notebook_to_model

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,14 @@
-# Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+import pickle
+from pathlib import Path
+from typing import Any
+
+
+def load_pickle(path: Path):
+    with open(path, "rb") as f:
+        loaded_obj = pickle.load(f)
+    return loaded_obj
+
+
+def save_pickle(path: Path, obj: Any):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
